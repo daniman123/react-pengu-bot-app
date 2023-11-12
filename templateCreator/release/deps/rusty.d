46061cba7d4db4b8b57924b7b5ac197d4297529e
daniman123 <daniel-saleh@hotmail.com>
2023-11-12 14:17:14 +0100

    build: "added templateCreator widget"

---
+++ templateCreator/release/deps/rusty.d
@@ -0,0 +1,6 @@
+C:\Users\Danie\Desktop\rusty\target\release\deps\rusty.exe: src\main.rs src\file_constants.rs
+
+C:\Users\Danie\Desktop\rusty\target\release\deps\rusty.d: src\main.rs src\file_constants.rs
+
+src\main.rs:
+src\file_constants.rs:
---
+++ templateCreator/release/rusty.d
@@ -0,0 +1,1 @@
+C:\Users\Danie\Desktop\rusty\target\release\rusty.exe: C:\Users\Danie\Desktop\rusty\src\file_constants.rs C:\Users\Danie\Desktop\rusty\src\main.rs
