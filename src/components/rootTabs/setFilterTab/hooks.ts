4fc6755074817dc8d0108ce4c4c7611552396ee3
daniman123 <69776685+daniman123@users.noreply.github.com>
2023-11-16 04:15:06 +0100

    Update hooks.ts

---
+++ src/components/rootTabs/setFilterTab/hooks.ts
@@ -0,0 +1,16 @@
+export const useFilterData = () => {
+    const [filterData, setFilterData] = useState<FilterData>({
+    cardTypes: {
+      rarity: "",
+      gender: "",
+      league: "",
+      nationality: "",
+      position: "",
+      team: "",
+      player: "",
+    },
+    priceRange: { max: 0, min: 0 },
+  });
+
+    return {filterData, setFilterData}
+}
