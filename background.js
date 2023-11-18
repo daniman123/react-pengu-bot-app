16c7c60bb84b966b38f433b60de0f2e144ad0ac5
daniman123 <>
2023-11-18 04:35:15 +0100

    .

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
 // eslint-disable-next-line no-undef
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	// if (
-	// 	tab.url &&
-	// 	tab.url.includes(
-	// 		"https://www.ea.com/ru-ru/ea-sports-fc/ultimate-team/web-app/"
-	// 	)
-	// ) {
-		// eslint-disable-next-line no-undef
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id },
-			files: ["contentScript.js"],
-		});
-	// }
+chrome.action.onClicked.addListener((tab) => {
+  if (
+    tab.url &&
+    tab.url.includes(
+      "https://www.ea.com/ru-ru/ea-sports-fc/ultimate-team/web-app/"
+    )
+  ) {
+    // eslint-disable-next-line no-undef
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["contentScript.js"],
+    });
+  }
+});
+
+// eslint-disable-next-line no-undef
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.buttonClicked) {
+    console.log("Button was clicked in content script");
+    // Perform background script actions here
+  }
+  // Optionally send a response back to the content script
+  sendResponse({ message: "Message received in background script" });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 // import PrimaryLayout from "./components/layout/primaryLayout/PrimaryLayout";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function App() {
-	useEffect(() => {
-		document.addEventListener("click", function (event) {
-			// Send a message to the background script
-			console.log("keys to tha bimmer");
-			// eslint-disable-next-line no-undef
-			// chrome.runtime.sendMessage("bbbgmlodlbfklpcpphnbpbfgbefhmjjp", {
-			// 	type: "clickEvent",
-			// 	content: "Click detected",
-			// });
+	// useEffect(() => {
+	// 	document.addEventListener("click", function (event) {
+	// 		// Send a message to the background script
+	// 		console.log("keys to tha bimmer");
+	// 		// eslint-disable-next-line no-undef
+	// 		// chrome.runtime.sendMessage("bbbgmlodlbfklpcpphnbpbfgbefhmjjp", {
+	// 		// 	type: "clickEvent",
+	// 		// 	content: "Click detected",
+	// 		// });
 
 			
-		});
-	}, []);
+	// 	});
+	// }, []);
 
 	return (
 		<></>
